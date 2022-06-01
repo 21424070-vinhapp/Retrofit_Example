9427152468b483ee42c73f246c19b8cc87c1df2b
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-06-01 18:00:01 +0700

    finish call API

--- app/src/main/java/com/example/retrofit_example/MainActivity.java
+++ app/src/main/java/com/example/retrofit_example/MainActivity.java
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.retrofit_example.api.ApiService;
 import com.example.retrofit_example.databinding.ActivityMainBinding;
+import com.example.retrofit_example.model.Currency;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding mBinding;
+
+    //link api: http://apilayer.net/api/live?access_key=843d4d34ae72b3882e3db642c51e28e6&currencies=VND&source=USD&format=1
+    private final String access_key = "843d4d34ae72b3882e3db642c51e28e6";
+    private final String currencies = "VND";
+    private final String source = "USD";
+    private final int format = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addControll();
 
         mBinding.btnCallAPI.setOnClickListener(new View.OnClickListener() {
@@ -23,11 +38,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onClickCallApi() {
+        ApiService.apiService
+                .convertUsdToVnd(access_key, currencies, source, format)
+                .enqueue(new Callback<Currency>() {
+                    @Override
+                    public void onResponse(Call<Currency> call, Response<Currency> response) {
+                        Toast.makeText(MainActivity.this, "Call API success", Toast.LENGTH_SHORT).show();
 
+                        Currency currency=response.body();
+                        if(currency!=null && currency.isSuccess())
+                        {
+                            mBinding.txtTerms.setText(currency.getTerms());
+                            mBinding.txtSource.setText(currency.getSource());
+                            mBinding.txtUsdVnd.setText(String.valueOf(currency.getQuotes().getUsdVnd()));
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Currency> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Call API error", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void addControll() {
-        mBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
     }
 }
